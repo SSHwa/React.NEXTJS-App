0279e32548a84bbfb437604ca626e30bfc6e12d8
showjf <showjf2@gmail.com>
2022-10-03 12:11:31 +0900

    md 파일 데이터 가져오는 함수 작성

---
+++ lib/post.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getSortedPostsData() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  const allPostsData = fileNames.map((fname) => {
+    const id = fname.replace(/\.md$/, "");
+    const fullPath = path.join(postsDirectory, fname);
+    const fileContents = fs.readFileSync(fullPath, "utf-8"); //내용
+
+    const matterResult = matter(fileContents);
+
+    return {
+      id,
+      ...(matterResult.data as { date: string; title: string }),
+    };
+  });
+
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
